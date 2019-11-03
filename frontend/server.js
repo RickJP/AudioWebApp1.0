225e3ebb73be1b2f5633147ac8d0b6a08fc24010
Rick <{ID}+{username}@users.noreply.github.com>
2019-11-03 19:41:04 +0900

    FIX: Messed it up; hopefully it works now

--- frontend/server.js
+++ frontend/server.js
@@ -6,16 +6,12 @@ const app = express();
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'build')));
  
-app.get('/audio', (req, res) => {
-   res.sendFile(path.join(__dirname, 'build', 'audio.html'));
-});
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
- 
 const PORT = process.env.PORT || 3000;
  
 app.listen(PORT, () => {
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
-import PlayAudio from "./students/OLD/PlayAudio";
-import RecordAudio from "./students/OLD/RecordAudio";
 import SpeakingTest from "./students/SpeakingTest";
 
 import Home from "./core/Home";
@@ -11,27 +9,27 @@ import PrivateRoute from "./auth/PrivateRoute";
 import Dashboard from "./user/UserDashboard";
 import AdminRoute from "./auth/AdminRoute";
 import AdminDashboard from "./user/AdminDashboard";
-// import AddCategory from "./admin/AddCategory";
-// import AddProduct from "./admin/AddProduct";
-// import Shop from "./core/Shop";
-// import Product from "./core/Product";
-// import Cart from "./core/Cart";
-// import Orders from "./admin/Orders";
+import AddCategory from "./admin/AddCategory";
+import AddProduct from "./admin/AddProduct";
+import Shop from "./core/Shop";
+import Product from "./core/Product";
+import Cart from "./core/Cart";
+import Orders from "./admin/Orders";
 import Profile from "./user/Profile";
-// import ManageProducts from "./admin/ManageProducts";
-// import UpdateProduct from "./admin/UpdateProduct";
+import ManageProducts from "./admin/ManageProducts";
+import UpdateProduct from "./admin/UpdateProduct";
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home} />
-                {/* <Route path="/shop" exact component={Shop} /> */}
+                <Route path="/shop" exact component={Shop} />
                 <Route path="/signin" exact component={Signin} />
                 <Route path="/signup" exact component={Signup} />
                
-                {/* <PrivateRoute path="/playAudio" exact component={PlayAudio} />
-                <PrivateRoute path="/recordAudio" exact component={RecordAudio} /> */}
+                {/* <PrivateRoute path="/playAudio" exact component={PlayAudio} /> */}
+                {/* <PrivateRoute path="/recordAudio" exact component={RecordAudio} /> */}
                 <PrivateRoute path="/speakingTest" exact component={SpeakingTest} />
                 {/* <PrivateRoute path="/rec" exact component={Rec} /> */}
                 <PrivateRoute
@@ -44,7 +42,7 @@ const Routes = () => {
                     exact
                     component={AdminDashboard}
                 />
-                {/* <AdminRoute
+                <AdminRoute
                     path="/create/category"
                     exact
                     component={AddCategory}
@@ -54,24 +52,24 @@ const Routes = () => {
                     exact
                     component={AddProduct}
                 /> */}
-                {/* <Route path="/product/:productId" exact component={Product} />
+                <Route path="/product/:productId" exact component={Product} />
                 <Route path="/cart" exact component={Cart} />
-                <AdminRoute path="/admin/orders" exact component={Orders} /> */}
+                <AdminRoute path="/admin/orders" exact component={Orders} />
                 <PrivateRoute
                     path="/profile/:userId"
                     exact
                     component={Profile}
                 />
-                {/* <PrivateRoute
+                <PrivateRoute
                     path="/admin/products"
                     exact
                     component={ManageProducts}
-                /> */}
-                {/* <AdminRoute
+                />
+                <AdminRoute
                     path="/admin/product/update/:productId"
                     exact
                     component={UpdateProduct}
-                /> */}
+                />
             </Switch>
         </BrowserRouter>
     );
